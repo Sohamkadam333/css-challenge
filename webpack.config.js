905838fdced0ee55f4a66facd2cbb3e79a79a839
soham kadam <sohamkadam58@gmail.com>
2022-05-03 19:24:34 +0530

    webpack.config.js added

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(scss|css)$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: 'file-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			inject: true,
+			template: path.resolve(__dirname, 'src', 'index.html'),
+		}),
+	],
+};
